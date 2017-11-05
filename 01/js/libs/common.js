926818591ecfa8ed50e34ebd2fca25582d67d09a
wwwx <risin_wangx@outlook.com>
2017-11-05 19:44:07 +0800

    update

--- 01/js/libs/common.js
+++ 01/js/libs/common.js
@@ -1,14 +1,212 @@
-define(function(){
+define(['zepto'], function(){
+
+	// 扩展Array
+	 $.extend(Array.prototype, {
+        indexOf: function(val) {
+            for (var i = 0; i < this.length; i++) {
+                if (this[i] == val)
+                    return i
+            }
+            return -1
+        },
+        remove: function(val) {
+            var index = this.indexOf(val);
+            if (index > -1) {
+                return this.splice(index, 1)
+            }
+        }
+    });
+
+	// 扩展String 
+    $.extend(String.prototype, {
+        trim: function() {
+            return this.replace(/(^\s*)|(\s*$)/g, '')
+        },
+        format: function() {
+            var result = this;
+            if (arguments.length > 0) {
+                var parameters = (arguments.length == 1 && $.isArray(arguments[0])) ? arguments[0] : $.makeArray(arguments);
+                $.each(parameters, function(i, n) {
+                    result = result.replace(new RegExp("\\{" + i + "\\}","g"), n)
+                })
+            }
+            return result
+        },
+        substitute: function(data) {
+            if (data && typeof (data) == 'object') {
+                return this.replace(/\{([^{}]+)\}/g, function(match, key) {
+                    var value = data[key];
+                    return (value !== undefined) ? '' + value : ''
+                })
+            } else {
+                return this.toString()
+            }
+        },
+        parseJSON: function() {
+            return (new Function("return " + this.toString()))()
+        },
+        parseDate: function() {
+            return (new Date()).parse(this.toString())
+        },
+        replaceAll: function(os, ns) {
+            return this.replace(new RegExp(os,'gm'), ns)
+        },
+        parseAttrJSON: function() {
+            var d = {}
+              , a = this.toString().split(';');
+            for (var i = 0; i < a.length; i++) {
+                if (a[i].trim() === '' || a[i].indexOf(':') < 1)
+                    continue;
+                var item = a[i].sliceBefore(':').trim()
+                  , val = a[i].sliceAfter(':').trim();
+                if (item !== '' && val !== '')
+                    d[item.toCamelCase()] = val._toRealValue()
+            }
+            return d
+        },
+        padLeft: function(width, ch) {
+            if (this.length >= width)
+                return this.toString();
+            return this._pad(width, ch, 0)
+        },
+        _pad: function(width, ch, side) {
+            var str = [side ? '' : this, side ? this : ''];
+            while (str[side].length < (width ? width : 0) && (str[side] = str[1] + (ch || ' ') + str[0]))
+                ;
+            return str[side]
+        },
+        isMobile: function() {
+            return ( new RegExp(/^(13|14|15|17|18)\d{9}$/).test(this.trim()))
+        },
+        sliceAfter: function(str) {
+            return (this.indexOf(str) >= 0) ? this.substring(this.indexOf(str) + str.length, this.length) : ''
+        },
+        sliceBefore: function(str) {
+            return (this.indexOf(str) >= 0) ? this.substring(0, this.indexOf(str)) : ''
+        },
+        escapeReg: function() {
+            return this.replace(new RegExp("([.*+?^=!:\x24{}()|[\\]\/\\\\])","g"), '\\\x241')
+        },
+        getQueryValue: function(name) {
+            var reg = new RegExp("(^|&|\\?|#)" + name.escapeReg() + "=([^&]*)(&|\x24)","");
+            var match = this.match(reg);
+            return (match) ? match[2] : ''
+        }
+    });
+
+    // 判断浏览器类型方法
+    $.browser = $.browser || {};
+    $.extend($.browser, (function() {
+        var ua = navigator.userAgent.toLowerCase(), os, version;
+        if (ua.indexOf('uleapp/') > 0) {
+            version = ua.sliceAfter('uleapp/').split('_')[3];
+            os = ua.sliceAfter('uleapp/').sliceBefore('_');
+            if (ua.sliceAfter('uleapp/').split('_')[1] == 'ule') {
+                var uappType = {
+                    ule: true,
+                    ylxd: false,
+                    ysh: false,
+                    hrysh: false
+                }
+            } else if (ua.sliceAfter('uleapp/').split('_')[1] == 'ysh') {
+                var uappType = {
+                    ule: false,
+                    ylxd: false,
+                    ysh: true,
+                    hrysh: false
+                }
+            } else if (ua.sliceAfter('uleapp/').split('_')[1] == 'hrysh') {
+                var uappType = {
+                    ule: false,
+                    ylxd: false,
+                    ysh: false,
+                    hrysh: true
+                }
+            } else {
+                var uappType = {
+                    ule: false,
+                    ylxd: true,
+                    ysh: false
+                }
+            }
+            var appobj = $.extend({
+                ios: os == 'ios',
+                android: os == 'android',
+                version: version
+            }, uappType);
+            return appobj
+        } else if (ua.indexOf('uzgapp/') > 0) {
+            version = ua.sliceAfter('uzgapp/').split('_')[3];
+            os = ua.sliceAfter('uzgapp/').sliceBefore('_');
+            return {
+                uzg: true,
+                wx: false,
+                ios: os == 'ios',
+                android: os == 'android',
+                version: version
+            }
+        } else {
+            return {
+                ule: false,
+                uzg: false,
+                ylxd: false,
+                ysh: false,
+                hrysh: false,
+                wx: ua.match(/micromessenger/i),
+                ios: ua.match(/(iphone|ipod|ipad);?/i),
+                android: ua.match(/android/i)
+            }
+        }
+    })());
+
+    // 分享
+    var shareCall = function() {
+        var title = "邮乐爽11购物节，百万爆款随你抢，亿万好券任你领，邀你一起来狂欢！"
+          , content = "邮乐爽11购物节，百万爆款随你抢，亿万好券任你领，邀你一起来狂欢！"
+          , imgUrl = "//i0.ulecdn.com/i/event/2017/1111/h5index/index_log.jpg"
+          , linkUrl = location.href + '&ulespring=true';
+        var linkStr = title + "##" + content + "##" + imgUrl + "##" + linkUrl + "&&WX##WF##QQ";
+        if ($.browser.android) {
+            window.group.jsMethod(linkStr)
+        } else if ($.browser.ios) {
+            return linkStr
+        } else {
+            return linkStr
+        }
+    };
+    window.shareCall = shareCall;
+
+    // 黑色小弹框
+    var toast = (function(){
+        var _toast, _timer;
+        return function(msg){
+            if(!msg) return;
+            _toast && clearTimeout(_timer);
+            if(!_toast){
+                _toast = $('<div class="toast-wrap"><div class="toast"></div></div>');
+                _toast.appendTo('body');
+            }
+            _toast.show().children('.toast').html(msg).addClass('toast-in');
+            _timer = setTimeout(function(){
+                if(!_toast) return;
+                _toast.children('.toast').removeClass('toast-in');
+                setTimeout(function(){
+                    _toast.hide();
+                }, 500)
+            }, 2000)
+        }
+    }());
+    window.toast = toast;
 
 	// 数据请求
 	var Ajax = {
 		needLoading: false,
 		execute: function (options) {
-			var _default = {
+			var o = $.extend({}, {
 				type: 'GET'
 				,url: ''
 				,async: true
-				,cache: false
+				,cache: true
 				,timeout: 3000
 				,jsonp: ''
 				,jsonpCallback: ''
@@ -16,10 +214,9 @@ define(function(){
 				,data: {}
 				,success: function() {}
 				,complete: function () {}
-			};
-			var o = $.extend({}, _default, options);
+			}, options);
 			$.ajax({
-				type: 'POST'
+				type: o.type
 				,url: o.url
 				,cache: o.cache
 				,async: o.async
@@ -35,19 +232,21 @@ define(function(){
 				}
 				,data: o.data
 				,dataType: 'jsonp'
-				,jsonp: o.jsonp
-				,jsonpCallback: o.jsonpCallback
-				,headers: o.headers
-				,success: function (data) {
-					o.success(data);
-					// if (data.error_code) {
-					// 	toast(data.error_msg);
-					// } else {
-					// 	if (data.data) {
-					// 		data = data.data;
-					// 	}
-					// 	o.success(data);
-					// }
+                ,jsonp: "jsonApiCallback"
+                ,jsonpCallback: o.jsonpCallback
+                ,headers: {
+                    "Accept-Encoding": "gzip,deflate"
+                }
+				,timeout: o.timeout
+				,success: function (res) {
+                    if (typeof res === 'string') {
+                        res = JSON.parse(res);
+                    }
+                    if (res.errorMsg) {
+                        tipBox(res.errorMsg)
+                    } else {
+                        o.success(res);
+                    }
 				}
 				,error: this.errorHandler
 				,complete: function () {
@@ -57,7 +256,7 @@ define(function(){
 				}
 			});
 		},
-		errorHandler: function (status) {
+		errorHandler: function (jqXHR, textStatus, errorThrown) {
 			// var errorCodes = {
 			// 	2: '接收到了错误的数据类型。'
 			// 	,4: '您访问的页面不存在。'
@@ -82,6 +281,7 @@ define(function(){
 
 
 	return {
-		Ajax: Ajax
+		Ajax: 	Ajax,
+		toast: toast
 	}
 })
--- 01/js/main.js
+++ 01/js/main.js
@@ -1,4 +1,3 @@
-
 require.config({
 	shim: {
 		// "math": {
@@ -7,69 +6,209 @@ require.config({
 		// }
 	},
 	paths: {
-		// "jquery": "jquery-3.2.1",
-		"Zepto": "libs/zepto.min",
-		"Common": "libs/common",
-		"Vue": "libs/vue"
+		"zepto": "libs/zepto.min",
+		"zepto-lazyload": "libs/zepto-lazyload.min",
+		"common": "libs/common",
+		"vue": "libs/vue"
 	}
 });
 
-require(['Zepto', 'Common', 'Vue'], function ($, __, Vue) {
-	// console.log($)
-	// console.log(math.add(1,2));
-
+require(['zepto', 'common', 'vue', 'zepto-lazyload'], function ($, __, Vue) {
+	//
 	var uleUrl = location.host.substring(location.host.indexOf('.') + 1);
 	var ulecdn = uleUrl.indexOf('beta') > -1 ? 'beta.ulecdn.com' : 'ulecdn.com';
     var url = location.href;
-    var shopUrl = '//m.ule.com/item/detail/'; //定义跳转到shop页面
 
+    // 列表模板
+    var item_li_0 = '<li class="{itemClass}"><a href="{itemUrl}">\
+	                    <div class="picBox">\
+	                        <img class="img" data-original="{imgUrl}" src="//i0.ulecdn.com/ulewap/i/290x290x2x.png"/>\
+	                    </div>\
+	                    <div class="desc">\
+	                        <p class="name">{listingName}</p>\
+	                        <p class="price clearfix"><i class="icon icon-price"></i>¥<span class="price-min">{minPrice}</span></p>\
+	                        <p class="buy"><span class="btn-buy">立即购买</span></p>\
+	                    </div>\
+	                </a></li>';
+
+	// 全局变量
+  	var __global = {
+  		Date: '11.1-11.11',
+  		isHideDate: false,
+  		bottomAdKey: ''
+  	}
+
+  	// 实例化Vue
 	var vm = new Vue({
 		el: '#app',
 		data: {
+	        keys: {
+	        	prds: ["11_pro_2_jxw", "11_pro_3_cp"],
+	        	stores: ["11_pro_3_rule"]
+	        },
+	        oDatas: {
+	        	prdsData: {},
+	        	storesData: {}
+	        },
 			api: {
 	            queryCouponList: "//prize." + uleUrl + "/mc/m/api/v2/base/coupon/couponListBySearch?",
 	            receiveCoupon: "//prize." + uleUrl + "/mc/m/api/v2/base/coupon/receiveCoupon",
 	            getPrdsUrl: "//static-content."+ ulecdn +"/mobilead/recommond/dwRecommond.do?restype=2001",
 	            getStoresUrl: "//static-content."+ ulecdn +"/mobilead/recommond/dwRecommond.do?restype=2002"
 	        },
-	        codes: {
-	        	prd: ["11_pro_2_jxw"]
-	        },
-			prds: {},
-			ads: {}
+	        shopUrl: '//m.ule.com/item/detail/', //定义跳转到shop页面
+	        
 		},
 		created: function() {
 
 		},
 		methods: {
 			init: function() {
-				this.getPrds();
+				var m = this;
+				m.province = url.getQueryValue('province');
+				if (m.province === 'jiangsu') {
+					__global.bottomAdKey = '919_pro_js_bottom';
+					m.keys.stores.push(__global.bottomAdKey);
+				}
+				if (m.province === 'jilin') {
+					__global.isHideDate = true;
+				}
+				m.getPrds(m.keys.prds.join(','));
+				m.getStores(m.keys.stores.join(','), m.renderStores);
 			},
-			getPrds: function() {
-            	// 定义vm变量，让它指向this,this是当前的Vue实例
-				var _vm = this;
+			getPrds: function(keyStr) {
+            	// 定义m变量，让它指向this,this是当前的Vue实例
+				var m = this;
 				__.Ajax.execute({
-					url: this.api.getPrdsUrl,
-	                data: {
-	                    moduleKeys: this.codes.prd.join(',')
+					url: m.api.getPrdsUrl,
+	                data: {	
+	                    moduleKeys: keyStr
 	                },
-	                jsonp: "jsonApiCallback",
-                	jsonpCallback: "jsonApiCallback1",
-	                headers: {
-	                    "Accept-Encoding": "gzip,deflate"
-	                },
-	                cache: true,
-	                success: function(res) {
-	                	// console.log(data);
-	                	// _vm.set(data, 'prd', JSON.parse(res));
-	                	_vm.prds = res;
-				console.log(_vm.prds)
-	                	_vm.renderPrds(res);
+	                jsonpCallback: "jsonApiCallback1",
+	                success: function(obj) {
+	                	for (var k in obj) {
+							var data = obj[k],
+								items = '',
+								container = '';
+							// 判断商品列表对应的父元素
+							switch (k) {
+								case m.keys.prds[0]:
+									container = '#shop01 > .list > ul';
+									break;
+								case m.keys.prds[1]: 
+									container = '#shop02 > .list > ul';
+									break;
+								default: ;
+							}
+							//
+							data.length = 4;
+							for (var i=0, len=data.length; i<len; i+=1) {
+								var item 		= data[i];
+			                    item.itemUrl 	= m.shopUrl + item.listingId;
+			                    item.imgUrl 	= item.imgUrl.replace(/^http(s)?:/, '');
+			                    items += item_li_0.substitute(item);
+							}
+
+							$(container).html(items);
+							document.addEventListener('DOMNodeInserted',function(){
+								$('.img').picLazyLoad({
+									effect: 'fadeIn',
+									threshold: '420'
+								})
+							},false);
+						}
 	                }
 				})
 			},
-			renderPrds: function(obj) {
-				console.log(obj);
+			getStores: function(keyStr, callback) {
+				var m = this;
+				__.Ajax.execute({
+					url: m.api.getStoresUrl,
+	                data: {	
+	                    sectionKeys: keyStr
+	                },
+	                jsonpCallback: "jsonApiCallback3",
+	                success: function(obj) {
+	                	$.extend(m.oDatas.storesData, obj);
+	                	(typeof callback === 'function') && callback();
+	                }
+				})
+			},
+			renderStores: function() { 
+				var m = this;
+				m.bottomAdList(); 
+				m.ruleList();  
+
+				m.$nextTick(function () {
+				  	// DOM 更新了
+				  	$('.img').picLazyLoad({
+						effect: 'fadeIn',
+						threshold: '420'
+					})
+				});
+			},
+			bottomAdList: function() {
+				var m = this,
+					data = m.oDatas.storesData[__global.bottomAdKey],
+                    items = '';
+                if (!data) {
+                	return false;
+                }
+                // for (var i=0; i<data.length; i++) {
+                //     items += '<li><a href="' + data[i].link + '"><img src="' + data[i].imgUrl.replace(/^http(s)?:/, '') + '" width="100%"/></a></li>'
+                // }
+                // $('#bottomAd ul').html(items);
+                Vue.component('item-list', {
+                	'template': '<ul><li v-for="item in data">{{ item.link }}</li></ul>',
+                	data: function() {
+                		return data
+                	}
+                });
+
+
+			},
+			ruleList: function() {
+				var m = this,
+					data = m.oDatas.storesData['11_pro_3_rule'];
+					rule_li = '';
+				if (!data) {
+                	return false;
+                }
+                for (var i=0, len=data.length; i<len; i++) {
+                    rule_li += '<li>' + data[i].title.replace(/&/g, '<br>') + '</li>'
+                }
+                $('.ruleBox').show(200).find('.ruleUl').html(rule_li);
+			}
+		},
+		components: {
+		  	'date-component': {
+				template: '<div class="act-date abs" v-bind:class="{ hide: isHideDate }">'
+					  	+    '活动时间<br>{{ Date }}'
+					  	+  '</div>',
+				data: function() {
+					return __global;
+				}
+			},
+			'ad-component': {
+				template: '<section id="bottomAd">'
+				      	+	'<div class="list">'
+				      	+     '<ul class="ul-style clearfix"><item-list></item-list></ul>'
+				      	+   '</div>'
+					    + '</section>',
+			    data: function() {
+			    	return __global;
+			    }
+			},
+			'rule-component': {
+				template: '<section class="ruleBox">'  
+				      	+ 	'<h3 class="ruleTitle">活动规则:</h3>'
+				      	+	'<div class="ruleCon">'
+				      	+     '<ul class="ruleUl"></ul>'
+				      	+   '</div>'
+					    + '</section>',
+			    data: function() {
+			    	return __global;
+			    }
 			}
 		},
 		mounted: function() {
@@ -78,7 +217,7 @@ require(['Zepto', 'Common', 'Vue'], function ($, __, Vue) {
 	})
 
 
-})	;
+});
 
 
 
